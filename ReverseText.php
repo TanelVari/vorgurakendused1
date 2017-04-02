59c45597acfaeebc7ee76e81270521db8210f79e
Tanel Vari <tavari@microsoft.com>
2017-04-02 21:07:36 +0300

    Kodutöö PHP. commit #1 (need to recover)

---
+++ ReverseText.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Kodune töö 4a</title>
+	<style type="text/css"></style>
+	<script>
+
+		window.onload = function(){
+			<?php
+				$my_string = "Seitsmendas loengus tutvusime PHPga";
+
+				for ($i=$my_string.length(); $i = 0 ; $i--)) {
+					echo "$mystring[$i]";
+				}
+		}
+
+	</script>
+</head>
+<body></body>
+</html>
